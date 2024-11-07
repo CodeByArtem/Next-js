3a03b090862835b321725cee7c36bf2f9957fefe
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-11-07 22:35:46 +0100

    Коммит изменений: Tailwind добавлен

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
 
     <main>
-      <h1>Home Page</h1>
+      <h1 className="text-xl">Home Page</h1>
       <ActiveLabel>Active</ActiveLabel>
       <NotActiveLabel>Not Active</NotActiveLabel>
     </main>
