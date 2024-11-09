5ae8ce8706fc18e597fde3b09669d1959ba84ca7
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-11-09 22:46:12 +0100

    Коммит изменений: start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
+import AddCompanyButton from '@/app/components/add-company-button';
 
-import StatusLabel, {Status} from '@/app/components/status-label';
+
 
 export default function Home() {
   return (
 
     <main>
       <h1 className="text-xl">Home Page</h1>
-  <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+<AddCompanyButton/>
     </main>
   );
 }
