86de2253731b436a6f750d4d94087e6e31098d2b
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-11-10 17:57:54 +0100

    Коммит изменений:Layaout

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -7,10 +7,8 @@ import CompanyTable from '@/app/components/company-table';
 import CompanyRow from '@/app/components/company-row';
 import { Status } from '@/app/components/status-label';
 
-
 export interface PageProps {
 }
-
 
 export default function Page({}: PageProps) {
   return (
@@ -21,8 +19,15 @@ export default function Page({}: PageProps) {
           <SearchInput />
         </Toolbar>
         <CompanyTable>
-          <CompanyRow id={1} category="Products" company="Costco" status={Status.Pending}
-                      promotion={true} country="USA" joinedDate="02.19.2023" />
+          <CompanyRow
+            id={1}
+            category="Products"
+            company="Costco"
+            status={Status.Pending}
+            promotion={true}
+            country="USA"
+            joinedDate="02.19.2023"
+          />
         </CompanyTable>
       </main>
     </>
